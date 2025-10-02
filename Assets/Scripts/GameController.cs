8e98cde9d4ac33533881d6ec4e1abf19065eca54
Boden Harris <180235143+bodenharris@users.noreply.github.com>
2025-10-02 15:57:33 -0400

    Replace repo with new Unity project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject playerTextPrefab; // Assign a Text prefab in Unity
+    public Transform playerTextContainer; // Parent object for text boxes
+
+    private Dictionary<int, TextMeshProUGUI> playerTexts = new Dictionary<int, TextMeshProUGUI>();
+
+    void Awake()
+    {
+        AirConsole.instance.onConnect += OnPlayerConnect;
+        AirConsole.instance.onDisconnect += OnPlayerDisconnect;
+        AirConsole.instance.onMessage += OnMessage;
+    }
+
+    void OnPlayerConnect(int device_id)
+    {
+        GameObject newTextObj = Instantiate(playerTextPrefab);
+        newTextObj.transform.SetParent(playerTextContainer, false);
+
+        // Use TMP component
+        TextMeshProUGUI textComp = newTextObj.GetComponent<TextMeshProUGUI>();
+        if (textComp == null)
+        {
+            Debug.LogError("TextMeshProUGUI component missing on prefab!");
+            return;
+        }
+
+        textComp.text = "Player " + device_id;
+        playerTexts[device_id] = textComp;
+    }
+
+
+    void OnPlayerDisconnect(int device_id)
+    {
+        // Remove the player's text box
+        if (playerTexts.ContainsKey(device_id))
+        {
+            Destroy(playerTexts[device_id].gameObject);
+            playerTexts.Remove(device_id);
+        }
+    }
+
+    void OnMessage(int from, JToken data)
+    {
+        Debug.Log("Message received from " + from + ": " + data.ToString());
+
+        if (data["name"] != null && playerTexts.ContainsKey(from))
+        {
+            playerTexts[from].text = data["name"].ToString();
+            Debug.Log("Updated text for player " + from + " to " + data["name"]);
+        }
+    }
+
+}
